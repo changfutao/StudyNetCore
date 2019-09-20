8efcd9254207c13518da55147e0bc97c62bb50dd
changfutao <changfutao1989@126.com>
2019-09-20 17:06:22 +0800

    ModelState

--- StudyNetCore.WebApp/Controllers/HomeController.cs
+++ StudyNetCore.WebApp/Controllers/HomeController.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudyNetCore.WebApp.Models;
 
 namespace StudyNetCore.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Index";
+        }
+        [HttpPost]
+        public IActionResult AddUser([FromForm]UserModel userModel)
+        {
+            if(userModel.UserName == "taoge")
+            {
+                ModelState.AddModelError("UserName", "用户名不能为taoge");
+            }
+            if (!ModelState.IsValid)
+            {
+                //StringBuilder errinfo = new StringBuilder();
+                //foreach (var item in ModelState.Values)
+                //{
+                //    foreach (var error in item.Errors)
+                //    {
+                //        errinfo.AppendFormat("{0}\\n", error.ErrorMessage);
+                //    }
+                //}
+
+                //var errorMessage=errinfo.ToString();
+                return BadRequest(ModelState);
+            }
+            return null;
         }
     }
 }
---
+++ StudyNetCore.WebApp/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyNetCore.WebApp.Models
+{
+    public class UserModel
+    {
+        [Display(Name="用户名")]
+        [Required(ErrorMessage ="{0}是必填项")]
+        [MaxLength(50,ErrorMessage ="{0}长度最大50")]
+        public string UserName { get; set; }
+        [Display(Name = "密码")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(50, ErrorMessage = "{0}长度最大50")]
+        [MinLength(6,ErrorMessage ="{0}密码最小6")]
+        public string Password { get; set; }
+    }
+}
