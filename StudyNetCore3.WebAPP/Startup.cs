c8a810fb15ec7e7b9e1f9544101a562e88427237
changfutao <changfutao1989@126.com>
2019-09-24 15:41:03 +0800

    asp.net core3.0

---
+++ StudyNetCore3.WebAPP/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace StudyNetCore3.WebAPP
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //注入传统的MVC
+            services.AddControllersWithViews();
+            //只是ResfulAPI(不需要Views)
+            //services.AddControllers();
+
+            //引入EFCore
+            //services.AddDbContext<>
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //静态文件中间件
+            app.UseStaticFiles();
+            //路由中间件
+            app.UseRouting();
+            //端点中间件
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                    );
+
+                //使用特性路由
+                //endpoints.MapControllers();
+            });
+  
+        }
+    }
+}
